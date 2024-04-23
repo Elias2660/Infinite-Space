ab4c36862bb4dd2450d8b446f8ec8f790e2989b9
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-04-23 09:59:28 -0400

    work

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,9 +9,9 @@ export const metadata: Metadata = {
   title: "Infinite Scroll Test",
   description: "Check out project on github!",
   icons: {
-    icon:  "/images/icons/favicon.ico",
-    shortcut:  "/images/icons/favicon-32x32.png",
-    apple: "/images/icons/apple-touch-icon.png",
+    icon:  process.env.NODE_ENV === 'production' ? '/Infinite-Space' : "" + "/images/icons/favicon.ico",
+    shortcut: process.env.NODE_ENV === 'production' ? '/Infinite-Space' : "" +  "/images/icons/favicon-32x32.png",
+    apple: process.env.NODE_ENV === 'production' ? '/Infinite-Space' : "" + "/images/icons/apple-touch-icon.png",
   }
 };
 
@@ -24,9 +24,9 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <base href={'/'} />
-        <link rel="apple-touch-icon" sizes="180x180" href={"/images/icons/apple-touch-icon.png"} />
-        <link rel="icon" type="image/png" sizes="32x32" href={"/images/icons/favicon-32x32.png"} />
-        <link rel="icon" type="image/png" sizes="16x16" href={"/images/icons/favicon-16x16.png"} />
+        <link rel="apple-touch-icon" sizes="180x180" href={process.env.NODE_ENV === 'production' ? '/Infinite-Space' : "" + "/images/icons/apple-touch-icon.png"} />
+        <link rel="icon" type="image/png" sizes="32x32" href={process.env.NODE_ENV === 'production' ? '/Infinite-Space' : "" + "/images/icons/favicon-32x32.png"} />
+        <link rel="icon" type="image/png" sizes="16x16" href={process.env.NODE_ENV === 'production' ? '/Infinite-Space' : "" + "/images/icons/favicon-16x16.png"} />
         <link rel="manifest" href={"/site.webmanifest"} />
         <link rel="mask-icon" href={"/images/icons/safari-pinned-tab.svg"} color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
